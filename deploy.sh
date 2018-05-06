5c067800d36b311e49b5c40748e49bc54c414a2e
Jens Walter <js.walter@gmx.net>
2018-05-06 17:59:10 +0200

    refactor to flows.app

--- deploy.sh
+++ deploy.sh
@@ -7,4 +7,4 @@ if [ -z "$1" ]
 fi
 
 aws cloudformation package --template-file cf.yml --s3-bucket $1 --output-template .packaged-template.yml $2 $3
-aws cloudformation deploy --template-file .packaged-template.yml --capabilities CAPABILITY_IAM --stack-name MESHIFY-sns $2 $3
+aws cloudformation deploy --template-file .packaged-template.yml --capabilities CAPABILITY_IAM --stack-name FLOWS-sns $2 $3
